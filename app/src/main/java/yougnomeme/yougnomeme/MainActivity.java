b1188a64bd0f735d7bcd27953fe76b481db6bd0c
will <patterson.wj@gmail.com>
2016-11-20 12:36:59 -0800

    Resize

--- app/src/main/java/yougnomeme/yougnomeme/MainActivity.java
+++ app/src/main/java/yougnomeme/yougnomeme/MainActivity.java
@@ -2,6 +2,10 @@ package yougnomeme.yougnomeme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpSeekBar();
+        ((ImageView)findViewById(R.id.uxImageView)).setScaleX((float)0.5);
+    }
+
+    private void setUpSeekBar() {
+        ((SeekBar)findViewById(R.id.uxSeekBar)).setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d("Progres", new Integer(progress).toString());
+                ((ImageView)findViewById(R.id.uxImageView)).setScaleX(((float)progress)/100);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
