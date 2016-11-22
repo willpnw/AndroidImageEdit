363e78fcf24b1460f6a20be66ad1ab14fd2a1cae
will <patterson.wj@gmail.com>
2016-11-21 19:00:37 -0800

    Camera App

--- app/src/main/java/yougnomeme/yougnomeme/MainActivity.java
+++ app/src/main/java/yougnomeme/yougnomeme/MainActivity.java
@@ -1,15 +1,22 @@
 package yougnomeme.yougnomeme;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1888;
 
     int alpha = 0;
     int red = 0;
@@ -21,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpSeekBars();
+        setupImageView();
     }
 
     private void setUpSeekBars() {
@@ -127,5 +135,23 @@ public class MainActivity extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
+    }
+
+    private void setupImageView() {
+        ((ImageView)findViewById(R.id.uxImageView)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "The favorite list would appear on clicking this icon", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            ((ImageView)findViewById(R.id.uxImageView)).setImageBitmap(photo);
+        }
     }
 }
